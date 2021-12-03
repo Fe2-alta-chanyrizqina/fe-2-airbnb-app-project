8ace222e79e416b2f45f7816a585d6b4588661bf
AidaFna <93112554+AidaFna@users.noreply.github.com>
2021-12-03 23:20:37 +0700

    add routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-
-import React from "react";
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 import Home from './views/Home/home.jsx'
@@ -7,33 +5,26 @@ import HomeStay from './views/ListStay/listStay.jsx'
 import HomeDetails from './views/ListStay/homeDetail';
 import Payment from './views/Payment/payment';
 import Trip from './views/Trip/trip';
-import Profile from './views/Profile/profile.jsx'
-import ProfileEdit from './views/Profile/profileEdit.jsx'
-import DetailMyhomestay from './views/MyHomestay/detailMyhomestay'
+import Login from './components/login';
+import Register from './components/register';
+import ProfileEdit from './views/Profile/profileEdit';
+import Profile from './views/Profile/profile';
+
 
 const App = () => {
-
-  return (
-  <Router >
-
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-
-      <Route path="/profile/:id" element={<Profile />}/>
-      <Route path="/profileEdit/:id" element={<ProfileEdit />}/>
-
-      <Route path="/homes" element={<HomeStay/>}/>
-      <Route path="/rooms/:id" element={<HomeDetails/>}/>
-      <Route path="/book/stays/:id" element={<Payment/>}/>
-      <Route path="/trips" element={<Trip/>}/>
-
-      <Route path="/myhomes/:id" element={<DetailMyhomestay/>}/>
-
-    </Routes>
-    </Router>
-  );
-}
-
+  return  <Router>
+            <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/homes" element={<HomeStay/>}/>
+              <Route path="/rooms/:id" element={<HomeDetails/>}/>
+              <Route path="/book/stays/:id" element={<Payment/>}/>
+              <Route path="/trips" element={<Trip/>}/>
+              <Route path="/login" element={<Login />}/>
+              <Route path="/register" element={<Register />}/>  
+              <Route path="/profile" element={<Profile />}/>
+              <Route path="/profileEdit" element={<ProfileEdit />}/>
+            </Routes>
+          </Router>
 
 }
 export default App;
